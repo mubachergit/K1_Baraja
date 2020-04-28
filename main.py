157a0e915918bfcd918697400766117aef69f7b3
mubacher <mubacheralrifai@gmail.com>
2020-04-28 15:11:06 +0200

    Baraja en funcion

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-palos = ('o', 'c', 'e', 'b')
-cartas = ('A','2','3','4','5','6','7','S','C','R')
+p = ('o', 'c', 'e', 'b')
+c = ('A','2','3','4','5','6','7','S','C','R')
 
-baraja = []
+pf = ('D', 'P', 'T', 'C')
+cf = ('A','2','3','4','5','6','7','8','9','10', 'J', 'Q', 'K')
 
-for palo in palos:
-    for carta in cartas:
-        naipe = carta + palo
-        baraja.append(naipe)
 
-print(baraja)
+def crea_baraja(palos, cartas):
+    
+    baraja = []
+    for palo in palos:
+        for carta in cartas:
+            naipe = carta + palo
+            baraja.append(naipe)
+
+    return baraja
+
+baraja1 = crea_baraja(p,c)
+baraja2 = crea_baraja(pf, cf)
+print(baraja1)
+print(baraja2)
